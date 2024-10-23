cd4a691ca4c0ed90dad38c1ffcc4b2e048509f8c
aleksanderlukic <aleksander.lukic@edu.newton.se>
2024-10-23 19:17:40 +0200

    Jag har skapat en mapp för alla klasser och ytterligare en till map med de olika kategorier i den mappen för att de ska bli mer väl strukturerat. Den första klassen jag skrev var Bas klassen och Person

---
+++ FitTrack/Classes/BaseClasses/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Classes.BaseClasses
+{
+    public class Person
+    {
+        public string Username { get; set; }    
+        public string Password { get; set; }
+
+        public virtual void SignIn()
+        {
+            Console.WriteLine("Sign in as: " + Username);
+        }
+    }
+}
