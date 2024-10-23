db13c1ae0d8f95e1b0ee8359d1c3db7d1b1e88fd
aleksanderlukic <aleksander.lukic@edu.newton.se>
2024-10-23 19:54:53 +0200

    Jag har skapat en User katerogi nu i Base Class

---
+++ FitTrack/Classes/BaseClasses/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace FitTrack.Classes.BaseClasses
+{
+    public class User : Person 
+    {
+        public string Country { get; set; }
+        public string SecurityQuestion { get; set; }
+
+        public string SecurityAnswer { get; set; }
+
+        public override void SignIn()
+        {
+            Console.WriteLine("User signed in: " + Username);
+        }
+
+        public void ResetPassword(string securityAnswer)
+
+        {
+            if (securityAnswer == SecurityAnswer)
+
+            {
+                Console.WriteLine("Password reset successful.");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Incorecct security answer.");
+            }
+
+        }
+
+    }
+
+
+
+
