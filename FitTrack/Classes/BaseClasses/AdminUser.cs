649a27797ed12c19484b2c14224453f0cfa7889e
aleksanderlukic <aleksander.lukic@edu.newton.se>
2024-10-24 08:59:41 +0200

    Jag har skapat en WorkoutDetailsWindow

---
+++ FitTrack/Classes/BaseClasses/AdminUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Classes.BaseClasses
+{
+
+    public class AdminUser : User
+    {
+        public void ManageAllWorkouts(List<Workout> workouts)
+        {
+            foreach (var workout in workouts)
+            {
+                Console.WriteLine("Managing workout: " + workout.Notes);
+            }
+        }
+    }
