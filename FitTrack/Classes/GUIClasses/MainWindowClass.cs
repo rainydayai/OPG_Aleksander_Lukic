27793e24e1ee285373f6fd687fc92d2fad5cc918
aleksanderlukic <aleksander.lukic@edu.newton.se>
2024-10-29 09:09:23 +0100

    Jag har lagt till en GUICLass map där jag lagt till en MainWindowClass och en RegisterWindowClass

---
+++ FitTrack/Classes/GUIClasses/MainWindowClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Classes.GUIClasses
+{
+    public class MainWindowClass
+    {
+        public string LabelTitle { get; set; }
+        public string UsernameInput { get; set; }
+        public string PasswordInput { get; set; }
+
+        public void SignIn()
+
+        {
+
+            Console.WriteLine("Inloggningsmetod anropad i MainWindow. ");
+        }
+
+        public void Register()
+        {
+            RegisterWindowClass registerWindow = new RegisterWindowClass();
+            registerWindow.RegisterNewUser();
+            
+        }
+    }
+}
+
+
+
+
+
